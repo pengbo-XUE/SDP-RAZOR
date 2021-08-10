6ecc0a03c06cdaccef09d444e7d9b3c45c81e960
Tim X <80139645+pengbo-XUE@users.noreply.github.com>
2021-08-10 18:41:54 +1000

    db to cshtml

--- SDP/Pages/ProductDisplay.cshtml.cs
+++ SDP/Pages/ProductDisplay.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,22 @@ namespace SDP.Pages
 {
     public class ProductDisplayModel : PageModel
     {
+        [BindProperty(Name = "productID", SupportsGet = true)]
+        public string productID { get; set; }
         public void OnGet()
         {
+            if (HttpContext.Session.GetString("Id") == null)
+            {
+                Models.GuestCustomer guest = new Models.GuestCustomer();
+                Global.guestList.Add(guest);
+                string Id = guest.ID.ToString();
+                HttpContext.Session.SetString("Id", Id);
+            }
+            ViewData["Id"] = HttpContext.Session.GetString("Id");
+        }
+        public void OnGetDisplay()
+        {
+            ViewData["ProductID"] = productID;
         }
     }
 }
